b610d88614992d941a0cd1a27e38fdfec1e98e72
lzhong13 <lzhong13@uw.edu>
2022-01-12 00:40:14 -0500

    finished JWT authentication

--- src/main/java/com/jwt/userservice/api/UserResource.java
+++ src/main/java/com/jwt/userservice/api/UserResource.java
@@ -1,5 +1,10 @@
 package com.jwt.userservice.api;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +14,19 @@ import com.jwt.userservice.service.*;
 import com.jwt.userservice.model.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api")
@@ -43,6 +59,47 @@ public class UserResource {
     public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
         userService.addRoleToUser(form.getUsername(), form.getRoleName());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authroizationHeader = request.getHeader(AUTHORIZATION);
+        if(authroizationHeader != null && authroizationHeader.startsWith("Bearer ")) {
+            try {
+                //Verify the token
+                String token = authroizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secretiveness".getBytes(StandardCharsets.UTF_8));
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodeJWT = verifier.verify(token);
+
+                //Get the userinfo
+                String username = decodeJWT.getSubject();
+                AppUser user = userService.getUser(username);
+
+                String accessToken = JWT.create()
+                        .withSubject(user.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis()  + 10 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles", user.getRoles().stream().map(AppRole::getName).collect(Collectors.toList()))
+                        .sign(algorithm);
+
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", accessToken);
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+
+            } catch (Exception exception) {
+                response.setHeader("error", exception.getMessage());
+                response.setStatus(403);
+
+                Map<String, String> error = new HashMap<>();
+                error.put("error_message", exception.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        } else {
+            throw new RuntimeException("Refresh token missing");
+        }
     }
 }
 
--- src/main/java/com/jwt/userservice/service/UserServiceImpl.java
+++ src/main/java/com/jwt/userservice/service/UserServiceImpl.java
@@ -5,19 +5,30 @@ import com.jwt.userservice.model.AppUser;
 import com.jwt.userservice.repo.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Collection;
+import java.util.ArrayList;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public AppUser saveUser(AppUser user) {
         log.info("Saving new user: {}", user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
@@ -45,5 +56,24 @@ public class UserServiceImpl implements UserService {
     public List<AppUser> getUsers() {
         log.info("Fetch all users");
         return userRepo.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = userRepo.findByUsername(username);
+
+        if (user == null) {
+            log.error("Does not found any user");
+            throw new UsernameNotFoundException("No user found");
+        } else {
+            log.info("Found user");
+        }
+
+        Collection<SimpleGrantedAuthority> authories = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authories.add(new SimpleGrantedAuthority(role.getName()));
+        });
+
+        return new User(user.getUsername(), user.getPassword(), authories);
     }
 }
